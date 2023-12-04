4417d6caebe45c6dd4d8aa7d40f37bac649824fd
Jungwoo <evencoding@gmail.com>
2023-12-04 15:31:48 +0900

    useSyncExternalStore: 브라우저상의 시간이 지남에 따라 변경되는 일부 값을 구독하려는 경우

---
+++ src/useSyncExternalStore/useOnlineStatus.ts
@@ -0,0 +1,20 @@
+import { useSyncExternalStore } from 'react';
+
+export function useOnlineStatus() {
+  const isOnline = useSyncExternalStore(subscribe, getSnapshot);
+  return isOnline;
+}
+
+const getSnapshot = () => {
+  return navigator.onLine;
+};
+
+const subscribe = (callback: () => void) => {
+  window.addEventListener('online', callback);
+  window.addEventListener('offline', callback);
+
+  return () => {
+    window.removeEventListener('online', callback);
+    window.removeEventListener('offline', callback);
+  };
+};
